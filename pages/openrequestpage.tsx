79375ed0c747ff774c0263a55599cdf4b38e98ec
Nikita <kofman.niki@gmail.com>
2023-10-25 14:59:38 +0200

    creation of opened request page

---
+++ pages/openrequestpage.tsx
@@ -0,0 +1,11 @@
+import Home from "../components/Home";
+import React from "react";
+import Concierge from "../components/Concierge";
+import Dashconcierge from "../components/Dashconcierge";
+import OpenRequest from "../components/OpenRequest";
+
+function OpenedRequestPage() {
+  return <OpenRequest />;
+}
+
+export default OpenedRequestPage;
