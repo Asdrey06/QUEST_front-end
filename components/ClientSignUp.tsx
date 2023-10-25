c1fc9a6e112968123d147e3ad58ae89d3a71ff7b
Audrey <audrey_reynaud@hotmail.fr>
2023-10-25 17:44:52 +0200

    sign up client css + fetch

--- components/ClientSignUp.tsx
+++ components/ClientSignUp.tsx
@@ -9,6 +9,7 @@ import {
   faArrowRight,
   faCheck,
 } from "../node_modules/@fortawesome/free-solid-svg-icons/index";
+import { Modal } from "antd";
 // import { faSolid } from "../node_modules/@fortawesome/free-solid-svg-icons/index";
 import Link from "../node_modules/next/link";
 import { useEffect, useState } from "react";
@@ -18,7 +19,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 function ClientSignUp() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   useEffect(() => {
+    const showModal = () => {
+      setIsModalVisible(!isModalVisible);
+      let userSection = (
+        <FontAwesomeIcon icon={faUser} onClick={() => showModal()} />
+      );
+    };
+
     // Load the Cloudinary widget script when the component is mounted
     const script = document.createElement("script");
     script.src = "https://widget.cloudinary.com/v2.0/global/all.js";
@@ -36,7 +46,7 @@ function ClientSignUp() {
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-
+  const [birthday, setBirthday] = useState("");
   const [wrongpw, setWrongPw] = useState("");
 
   const [id, setId] = useState("");
@@ -71,72 +81,82 @@ function ClientSignUp() {
       {/* HEADER START */}
       <Header />
       {/* HEADER END */}
-      <div className="h-full mt-20">
+
+      <div className="flex flex-col h-full mt-20">
         <div className="flex">
           {" "}
           <h1
-            className="pl-10  pb-5 pt-5 w-full mb-5 text-3xl font-semibold"
+            className="flex flex-col items-center justify-center pl-10 pb-3 pt-3 w-full mb-10 text-3xl font-semibold"
             style={{ color: "#68938B", backgroundColor: "#EFFDF5" }}
           >
             Créez votre compte client
           </h1>
         </div>
-        <div className="flex flex-row mb-5">
+        <div className="flex flex-col items-center justify-center w-full mb-5">
           {/* BLOC 1 */}
-          <div className="ml-10 flex flex-col p-3 w-4/12 bg-neutral-100 rounded-3xl">
+          <div className="flex items-center justify-center ml-15 w-80 mb-5 flex-col shadow-md p-3 bg-neutral-100 rounded-3xl">
             <input
               type="text"
-              className="mt-3 mb-3 border-2 w-4/12 p-2 rounded-xl border-neutral-500"
+              s
+              className="mt-3 mb-3 border-2 w-60 p-2 rounded-xl border-neutral-400"
               placeholder="Prénom"
               onChange={(e) => setFirstName(e.target.value)}
               value={firstName}
             />
-
             <input
               type="text"
-              className="ml-3 mt-3 mb-3 border-2 w-4/12 p-2 rounded-xl border-neutral-500"
+              className="mt-3 mb-3 border-2 w-60 p-2 rounded-xl border-neutral-400"
               placeholder="Nom"
               onChange={(e) => setLastName(e.target.value)}
               value={lastName}
             />
+            <input
+              type="text"
+              className="mt-3 mb-3 border-2 w-60 p-2 rounded-xl border-neutral-400"
+              placeholder="Date de naissance"
+              onChange={(e) => {
+                const inputValue = e.target.value;
+                const validatedInput = inputValue.replace(/[^0-9/]/g, "");
+                setBirthday(validatedInput);
+              }}
+              value={birthday}
+            />
+            <input
+              type="textarea"
+              className="mt-3 mb-3 border-2 w-60 p-2 rounded-xl border-neutral-400"
+              placeholder="E-mail"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />{" "}
+            <input
+              type="password"
+              className="mt-3 mb-3 border-2 w-60 p-2 rounded-xl border-neutral-400"
+              placeholder="Mot de passe"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+            {/* <div className="cursor-pointer border-2 pl-5 pr-5 pt-2 pb-2 flex items-center justify-center rounded-2xl w-full text-xl text-white"> */}
           </div>
-          <input
-            type="password"
-            className="ml-3 mt-3 mb-3 border-2 w-4/12 p-2 rounded-xl border-neutral-500"
-            placeholder="Mot de passe..."
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-        </div>
-        <div className="flex flex-row">
-          <input
-            type="textarea"
-            className="mt-3 mb-3 border-2 w-4/12 p-2 rounded-xl border-neutral-500"
-            placeholder="E-mail"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />{" "}
-        </div>
-        <div className="  flex justify-end">
           <div
-            className={`${styles.hovereffect} cursor-pointer border-2 pl-5 pr-5 pt-2 pb-2 flex items-center justify-center rounded-2xl w-full text-xl text-white`}
+            className={`${styles.hovereffect} flex flex-col shadow-md  items-center justify-center text-center mt-5 cursor-pointer border-2  pt-2 pb-2 mb-3 rounded-2xl w-60 text-xl text-white`}
             onClick={handleRegister}
           >
             Envoyer
           </div>
         </div>
-
-        {/* END OF BLOC 3  */}
       </div>
     </div>
   );
   {
-    /* FOOTER */
+    /* END OF BLOC 3  */
   }
-  <Footer />;
-  {
-    /* FOOTER END  */
-  }
+}
+{
+  /* FOOTER */
+}
+<Footer />;
+{
+  /* FOOTER END  */
 }
 
 export default ClientSignUp;
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { faUser } from "../node_modules/@fortawesome/free-solid-svg-icons/index"
 import { faInstagram } from "../node_modules/@fortawesome/free-brands-svg-icons/index";
 import { faFacebook } from "../node_modules/@fortawesome/free-brands-svg-icons/index";
 import { faCheck } from "../node_modules/@fortawesome/free-solid-svg-icons/index";
+import { faXmark } from "../node_modules/@fortawesome/free-solid-svg-icons/index";
 import Link from "../node_modules/next/link";
 import { useEffect, useState } from "react";
 
@@ -16,84 +17,95 @@ function Header() {
   };
 
   return (
-    <div
-      className={`fixed top-0 w-full flex flex-row items-center justify-between pr-10 pl-10 pb-5 pt-5 mb-10 ${
-        login ? "h-auto" : "h-64"
-      }`}
-      style={{ backgroundColor: "#33B49C" }}
-    >
-      <div className="flex flex-row items-center">
-        <Link href="/">
-          <img
-            src="/questlogowhite.png"
-            className="h-10 mr-10 cursor-pointer"
-          />
-        </Link>
-        {login ? (
-          <div className="w-48 border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
+    <div className="flex-col flex justify-start">
+      <div
+        className={`fixed top-0 w-full flex flex-row justify-between pr-10 pl-10 pb-5 pt-5 mb-10 `}
+        style={{ backgroundColor: "#33B49C" }}
+      >
+        <div className="flex flex-row">
+          <Link href="/">
+            <img
+              src="/questlogowhite.png"
+              className="h-10 mr-10 cursor-pointer"
+            />
+          </Link>
+
+          <div className="w-48 border-black h-10 pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
             <Link href="/conciergewelcome">Devenir concierge</Link>
           </div>
-        ) : null}
-      </div>
-
-      {login ? (
-        <div className="flex flex-row">
-          <div className="w-64 p-4">
-            <div className="text-white mb-2">Vos identifiants Client</div>
-            <input
-              type="text"
-              placeholder="E-mail"
-              className="w-full border border-gray-300 rounded-md mb-2"
-            />
-            <input
-              type="password"
-              placeholder="Mot de passe"
-              className="w-full border border-gray-300 rounded-md mb-4"
-            />
-            <button className="w-full border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
-              ENVOYER
-            </button>
-            <div style={{ marginBottom: "4mm" }}></div>
-            <button className="w-full border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
-              Login avec Google
-            </button>
-            <p className="text-white mt-4 text-center">ou</p>
-            <p className="text-white text-center">
-              <Link href="/clientsignuppage">Créez votre compte</Link>
-            </p>
-          </div>
-
-          <div className="w-64 p-4">
-            <div className="text-white mb-2">Vos identifiants Concierge</div>
-            <input
-              type="text"
-              placeholder="E-mail"
-              className="w-full border border-gray-300 rounded-md mb-2"
-            />
-            <input
-              type="password"
-              placeholder="Mot de passe"
-              className="w-full border border-gray-300 rounded-md mb-4"
-            />
-            <button className="w-full border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
-              ENVOYER
-            </button>
-            <div style={{ marginBottom: "4mm" }}></div>
-            <button
-              className="w-full border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600"
-              style={{ marginBottom: "4mm" }}
-            >
-              Devenir concierge
-            </button>
-          </div>
         </div>
-      ) : (
-        <FontAwesomeIcon
-          icon={faUser}
-          onClick={toggleLogin}
-          className="h-6 text-white cursor-pointer"
-        />
-      )}
+
+        {login ? (
+          <div className="flex flex-row w-9/12 items-center justify-end">
+            <div className="w-4/12 p-4 flex flex-col items-center justify-start border-r-2 border-r-emerald-200 pr-14">
+              <div className="text-white text-xl mb-3 mb-2">
+                Connexion compte client
+              </div>
+              <input
+                type="text"
+                placeholder="E-mail"
+                className="w-full border border-gray-300 rounded-md pb-2 pt-2 mb-3 pl-3"
+              />
+              <input
+                type="password"
+                placeholder="Mot de passe"
+                className="w-full border border-gray-300 rounded-md mb-4 pb-2 pt-2 mb-3 pl-3"
+              />
+              <button className="w-full border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
+                ENVOYER
+              </button>
+              <div style={{ marginBottom: "4mm" }}></div>
+              <button className="w-full border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
+                Login avec Google
+              </button>
+              <p className="text-white mt-2 mb-2 text-center">ou</p>
+              <p className="text-cyan-100 hover:text-cyan-300 text-center">
+                <Link href="/clientsignuppage">Créez votre compte</Link>
+              </p>
+            </div>
+            <div className="w-4/12 p-4 mb-4 flex flex-col items-center justify-start pl-14">
+              <div className="text-white mb-2 text-xl mb-3 ">
+                Connexion compte concierge
+              </div>
+              <input
+                type="text"
+                placeholder="E-mail"
+                className="w-full border border-gray-300 rounded-md mb-2 pb-2 pt-2 mb-3 pl-3"
+              />
+              <input
+                type="password"
+                placeholder="Mot de passe"
+                className="w-full border border-gray-300 rounded-md mb-4 pb-2 pt-2 mb-3 pl-3"
+              />
+              <button className="w-full border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
+                ENVOYER
+              </button>
+              <div className="text-white mt-5">ou</div>
+              <button
+                className="w-full pt-2 pb-2 flex items-center justify-center rounded-2xl text-cyan-100 hover:text-cyan-300 text-white"
+                style={{ marginBottom: "4mm" }}
+              >
+                Devenir concierge
+              </button>
+            </div>
+            <div
+              onClick={toggleLogin}
+              className="ml-10 flex justify-start h-full flex-row"
+            >
+              <FontAwesomeIcon
+                icon={faXmark}
+                className="h-6 text-slate-600 flex items-start"
+              />
+            </div>
+          </div>
+        ) : (
+          <FontAwesomeIcon
+            icon={faUser}
+            onClick={toggleLogin}
+            className="h-6 text-white cursor-pointer"
+          />
+        )}
+      </div>
     </div>
   );
 }
