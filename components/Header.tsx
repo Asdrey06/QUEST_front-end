03db5a38b74df806a4ee618401c224e8cdf0b4c1
erikaleon <erikaleon2@gmail.com>
2023-10-25 12:44:24 +0200

    modification heather -> loginClient loginConcierge

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,26 +9,64 @@ import Link from "../node_modules/next/link";
 import { useEffect, useState } from "react";
 
 function Header() {
+  const [login, setLogin] = useState(false);
+
+  const toggleLogin = () => {
+    setLogin(!login);
+  };
+
   return (
-    <div
-      className="fixed top-0 w-full flex flex-row items-center justify-between pr-10 pl-10 pb-5 pt-5 mb-10"
-      style={{ backgroundColor: "#33B49C" }}
-    >
+    <div className={`fixed top-0 w-full flex flex-row items-center justify-between pr-10 pl-10 pb-5 pt-5 mb-10 ${login ? "h-auto" : "h-64"}`} style={{ backgroundColor: "#33B49C" }}>
       <div className="flex flex-row items-center">
         <Link href="/">
-          <img
-            src="/questlogowhite.png"
-            className="h-10 mr-10 cursor-pointer"
-          />
+          <img src="/questlogowhite.png" className="h-10 mr-10 cursor-pointer" />
         </Link>
-        <button>
+        {login ? (
           <div className="w-48 border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
             <Link href="/conciergewelcome">Devenir concierge</Link>
           </div>
-        </button>
+        ) : null}
       </div>
 
-      <FontAwesomeIcon icon={faUser} className="h-6 text-white" />
+      {login ? (
+        <div className="flex flex-row">
+          <div className="w-64 p-4">
+            <div className="text-white mb-2">Vos identifiants Client</div>
+            <input type="text" placeholder="E-mail" className="w-full border border-gray-300 rounded-md mb-2" />
+            <input type="password" placeholder="Mot de passe" className="w-full border border-gray-300 rounded-md mb-4" />
+            <button className="w-full border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
+              ENVOYER
+            </button>
+            <div style={{ marginBottom: '4mm' }}></div>
+            <button className="w-full border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
+              Login avec Google
+            </button>
+            <p className="text-white mt-4 text-center">ou</p>
+            <p className="text-white text-center">
+              <Link href="/creercompte">Créez votre compte</Link>
+            </p>
+          </div>
+
+          <div className="w-64 p-4">
+            <div className="text-white mb-2">Vos identifiants Concierge</div>
+            <input type="text" placeholder="E-mail" className="w-full border border-gray-300 rounded-md mb-2" />
+            <input type="password" placeholder="Mot de passe" className="w-full border border-gray-300 rounded-md mb-4" />
+            <button className="w-full border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600">
+              ENVOYER
+            </button>
+            <div style={{ marginBottom: '4mm' }}></div>
+            <button className="w-full border-black pt-2 pb-2 flex items-center justify-center rounded-2xl text-sm text-white bg-neutral-800 hover:bg-neutral-600" style={{ marginBottom: '4mm' }}>
+              Devenir concierge
+            </button>
+          </div>
+        </div>
+      ) : (
+        <FontAwesomeIcon
+          icon={faUser}
+          onClick={toggleLogin}
+          className="h-6 text-white cursor-pointer"
+        />
+      )}
     </div>
   );
 }
