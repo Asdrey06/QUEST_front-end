1fb2a272ff870a6f3f9a72f53aa9fdc048f08211
Wilfried <wilfried.gomesfortes@gmail.com>
2023-10-27 11:03:32 +0200

    box design is done

--- components/RequestList.tsx
+++ components/RequestList.tsx
@@ -6,18 +6,19 @@ import { faCommentDots } from "@fortawesome/free-solid-svg-icons";
 function RequestList(props) {
   console.log("PROP", props);
   return (
-    <div className="bg-neutral-200 w-full hover:bg-neutral-300 m-10 p-10 rounded-2xl flex">
+    <div className="outline-black border-emerald-200 flex w-8/12 ml-10 mr-10 flex-wrap">
       {/* <div className={styles.imagesContainer}></div> */}
       {/* <div className={styles.textContainer}> */}
-      <ul>
-        <li>{props.instruction}</li>
-        <li>{props.serviceFees}</li>
-        <li>{props.productFees}</li>
-        <li>{props.totalFees}</li>
-        <li>{props.date}</li>
-        <li>{props.paymentInfo}</li>
-      </ul>
-      <p></p>
+      {/* <div className="bg-neutral-100 shadow-lg w-10/12 mb-5 ml-10 pt-4 pb-4 pl-4 rounded-md border-neutral-400 border-2 flex font-medium"> */}
+        <ul>
+          <li>Instruction: {props.instruction}</li>
+          <li>Service: {props.serviceFees}</li>
+          <li>Production: {props.productFees}</li>
+          <li>Total: {props.totalFees}</li>
+          <li>Date: {props.date}</li>
+          <li>Payment: {props.paymentInfo}</li>
+        </ul>
+      </div>
     </div>
     // </div>
   );
