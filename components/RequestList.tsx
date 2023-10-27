115f319b7f65ba61b5412a5392841583059928b3
Wilfried <wilfried.gomesfortes@gmail.com>
2023-10-27 13:03:04 +0200

    frontend dashboard concierge design done

--- components/RequestList.tsx
+++ components/RequestList.tsx
@@ -5,21 +5,30 @@ import { faCommentDots } from "@fortawesome/free-solid-svg-icons";
 
 function RequestList(props) {
   console.log("PROP", props);
+
+  const newDate = props.date.split("T")[0];
   return (
-    <div className="outline-black border-emerald-200 flex w-8/12 ml-10 mr-10 flex-wrap">
+    <div className="outline-black border-emerald-200 flex w-11/12 ml-10 mr-10 flex-wrap">
       {/* <div className={styles.imagesContainer}></div> */}
       {/* <div className={styles.textContainer}> */}
-      {/* <div className="bg-neutral-100 shadow-lg w-10/12 mb-5 ml-10 pt-4 pb-4 pl-4 rounded-md border-neutral-400 border-2 flex font-medium"> */}
+      <div className="bg-neutral-100 shadow-lg w-10/12 mb-5 ml-10 pt-4 pb-4 pl-4 rounded-md border-neutral-400 border-2 flex ">
         <ul>
-          <li>Instruction: {props.instruction}</li>
-          <li>Service: {props.serviceFees}</li>
-          <li>Production: {props.productFees}</li>
-          <li>Total: {props.totalFees}</li>
-          <li>Date: {props.date}</li>
-          <li>Payment: {props.paymentInfo}</li>
+          <li className="font-bold">
+            Instruction: <p className="text-3xl">{props.instruction}</p>
+          </li>
+          <li className="font-medium">Production: {props.productFees}</li>
+          <li className="font-medium">Date: {newDate}</li>
+          <div className="flex w-full">
+            {" "}
+            <li className="font-bold">Total: {props.totalFees}</li>
+            <li className="font-medium ml-80">
+              Commission: {props.serviceFees}
+            </li>
+          </div>
         </ul>
       </div>
     </div>
+
     // </div>
   );
 }
