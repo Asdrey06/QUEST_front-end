fb8b929408226bc68f525e28ce7c474bf1df1416
erikaleon <erikaleon2@gmail.com>
2023-10-27 17:42:22 +0200

    modification of the page OpenProfileConcierge and fetch

--- components/OpenProfileConcierge.tsx
+++ components/OpenProfileConcierge.tsx
@@ -15,50 +15,71 @@ import { useSelector } from "react-redux";
 
 
 function OpenProfileConcierge() {
-    return (
 
-<div className="min-h-screen bg-white flex flex-col">
-<Header />
-<div className="flex-grow flex flex-row">
-  <div className="flex flex-col w-full border-2 border-black mt-50">
-    <div className="mt-36 mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
-      Une place pour une photo d’identité
-    </div>
-    <div className="mt-36 mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
-      Description écrite par le concierge
-    </div>
-    <div className="mt-36 mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
-      Liste de compétences du concierge
-    </div>
-    <div className="mt-36 border-2 border-black p-4">
-      <button className="bg-header-color text-white">
-        Contactez "nom concierge"
-      </button>
-    </div>
-  </div>
-  <div className="flex flex-col w-full border-2 border-black">
-    <div className="text-white mb-4 border-2 border-black ml-10 mr-10 mt- p-2">
-      STATISTIQUE CONCIERGE
-      <br />
-      Pourcentage de satisfaction : 90%
-    </div>
-    <div className="mt-36 mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
-      DERNIERS AVIS CLIENTS...
+    const [conciergeList, setConciergeList] = useState([]);
 
-    </div>
-    <div className="mt-36 mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
-      DERNIERS AVIS CLIENTS...
+    useEffect(() => {
+        fetch("http://localhost:3000/concierges/conciergeList")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log();
+            // dispatch(
+            //   offersConcierge({
+            //     firstname: data.result[0].firstname,
+            //     username: data.result[0].username,
+            //   })
+            // );
     
-    </div>
-    <div className="mt-36 mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
-      DERNIERS AVIS CLIENTS...
+            setConciergeList(data.result);
+        });
+    }, []);
 
+console.log(conciergeList[0])
+
+
+return (
+    <div className="min-h-screen bg-white flex flex-col">
+    <Header />
+    <div className="flex-grow flex flex-row mt-40"> 
+        <div className="flex flex-col w-full border-2 border-black mt-50">
+        <div className="items-center text-xs flex-col flex justify-center ml-10 text-center text-neutral-500 rounded-lg w-32 h-32"
+                style={{ border: "3px solid #34B39C" }}
+    >
+    <img src="/trust.png" className="w-25 h-25"/>      
+            </div>
+
+        <div className="mt-36 mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
+        aboutme
+        </div>
+        <div className="mt-36 mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
+        skills
+          </div>
+          <div className="mt-36 border-2 border-black p-4">
+            <button className="bg-black text-white">
+              Contactez "concierge firstname"
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col w-full border-2 border-black">
+          <div className="text-black mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
+            STATISTIQUE CONCIERGE
+            <br />
+            Pourcentage de satisfaction : 90%
+          </div>
+          <div className="mt-36 mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
+            DERNIERS AVIS CLIENTS...
+          </div>
+          <div className="mt-36 mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
+            DERNIERS AVIS CLIENTS...
+          </div>
+          <div className="mt-36 mb-4 border-2 border-black ml-10 mr-10 mt-0 p-2">
+            DERNIERS AVIS CLIENTS...
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
-  </div>
-</div>
-<Footer />
-</div>
-);
+  );
 }
 
 export default OpenProfileConcierge;
